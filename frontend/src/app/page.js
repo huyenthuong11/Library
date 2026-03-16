2fd42fef980bfb97423a34e6202d41eeac81967b
huyenthuong11 <kmr22972@gmail.com>
2026-03-17 01:04:40 +0700

    create 1st version of home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,66 +1,41 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 
-export default function Home() {
+export default function Page() {
+  const router = useRouter();
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <div className={styles.header}>
+        <div className={styles.webicon}>
+          <div className={styles.logo}></div>
+          <div className={styles.websiteName}>LMS</div>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className={styles.sign}>
+          <a onClick={() => router.push("/login")}>Đăng Nhập</a>  
+          <a onClick={() => router.push("/register")}>Đăng Ký</a>
         </div>
-      </main>
-    </div>
-  );
+      </div>
+
+      <div className={styles.banner}>
+        <div className={styles.bannerFill}>
+          <div className={styles.headerBanner}>KHÁM PHÁ THẾ GIỚI TRI THỨC</div>
+          <div className={styles.fullName}>Hệ thống Quản lý thư viện</div>
+        </div>
+      </div>
+
+      <div className={styles.grid}>
+        <div className={styles.card}>SÁCH MỚI CẬP NHẬT</div>
+        <div className={styles.card}>DỊCH VỤ NỔI BẬT</div>
+        <div className={styles.card}>TIN TỨC & SỰ KIỆN</div>
+      </div>
+
+      <div className={styles.footer}>
+        <div className={styles.word}>Cầu Giấy, Hà Nội, Việt Nam</div>
+        <div className={styles.word}>Contact: 0912 xxx xxx</div>
+        <div className={styles.word}>Copyright</div>
+      </div>
+    </>
+  )
 }
+
