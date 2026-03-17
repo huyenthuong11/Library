5c0914a40854306f27f7c0ff84dafb3410597eb5
huyenthuong11 <kmr22972@gmail.com>
2026-03-17 12:00:02 +0700

    fix menu screen

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -32,7 +32,12 @@ export default function Page() {
         </div>
         <div className={styles.card}>
           <div className={styles.cardHeader}>
-            DỊCH VỤ NỔI BẬT
+            TOP 3 SÁCH ĐƯỢC MƯỢN NHIỀU NHẤT
+          </div>
+          <div className={styles.cardMain2}>
+            <div style={{border: '1px solid #8684849d', boxShadow: '0 0 1px 1px #868484bb', width: "30%", height: "100%", borderRadius: "8px"}}></div>
+            <div style={{border: '1px solid #f0a4a4b0', boxShadow: '0 0 1px 1px #f0a4a4b0', width: "30%", height: "100%", borderRadius: "8px"}}></div>
+            <div style={{border: '1px solid #868484b4', boxShadow: '0 0 1px 1px #868484bb', width: "30%", height: "100%", borderRadius: "8px"}}></div>
           </div>
         </div>
         <div className={styles.card}>
@@ -43,6 +48,7 @@ export default function Page() {
       </div>
 
       <div className={styles.footer}>
+        <div className={styles.word}>THƯ VIỆN CẦU GIẤY </div>
         <div className={styles.word}>Address: Cầu Giấy, Hà Nội, Việt Nam</div>
         <div className={styles.word}>Contact: 0912 xxx xxx</div>
         <div className={styles.word}>Copyright © Library System</div>
